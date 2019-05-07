abd734ac29eaf7d8bb9cebdc301f7d372722389c
VictorGus <retaow@gmail.com>
2019-05-07 16:45:39 +0300

    New project structure

--- index.js
+++ index.js
@@ -6,11 +6,52 @@ console.time("Execution time");
 const jsonFile = fs.readFileSync("/home/victor/Downloads/fhir.schema.json");
 const schema = JSON.parse(jsonFile);
 
-const testResource = {
-    resourceType:"Patient"
-};
+function InvalidArgument(message) {
+    this.name = 'InvalidSchemaArgument';
+    this.message = message || "Input argument is supposed to be schema";
+    this.stack = (new Error()).stack;
+}
 
-const resourceJSON = fs.readFileSync('/home/victor/Documents/Diploma/FHIR validation app/Resources/patient.json');
+function getUniqueErrors(arrayOfErrors, comp) {
+    const unique = arrayOfErrors
+          .map(e => e[comp])     // store the keys of the unique objects
+          .map((e, i, final) => final.indexOf(e) === i && i)     // eliminate the dead keys & store unique objects
+          .filter(e => arrayOfErrors[e]).map(e => arrayOfErrors[e]);
+    return unique;
+}
+
+function schemaToArrayOfKeys(schema, resource) {
+    if(typeof schema == "object" && (schema.$schema || schema.definitions)) {
+        return Object.keys(schema.definitions[`${resource.resourceType}`].properties);
+    } else {
+        throw new InvalidArgument();
+    }
+}
+
+function resourceToArrayOfKeys(resource) {
+    if(typeof resource == "object") {
+        return Object.keys(resource);
+    } else {
+        throw new InvalidArgument('Input argument is supposed to be resource');
+    }
+}
+
+function addUnrecordedErrors(unrecordedErrors, recordedErrors) {
+    for(let i = 0; i < unrecordedErrors.length; i++) {
+        recordedErrors.push(unrecordedErrors[i]);
+    }
+    return recordedErrors;
+}
+
+function findUnrecordedErrors(resource, schema, arrayOfErrors) {
+    var errorParams = [];
+    for(let i = 0; i < arrayOfErrors.length; i++) {
+        errorParams[i] = arrayOfErrors[i].params.additionalProperty;
+    }
+    return resource.filter(property => !schema.includes(property) && !errorParams.includes(property)); 
+}
+
+const resourceJSON = fs.readFileSync('/home/victor/Documents/Diploma/FHIR validation app/Resources/claim.json');
 const resourceToBeValidated = JSON.parse(resourceJSON);
 var validate = ajv.addSchema(schema).compile(schema);
 var valid = validate(resourceToBeValidated);
@@ -25,11 +66,5 @@ if(!valid) {
     }
     console.log(uniqueErrors.values());
 }
-
-console.log(validate.errors.length);
-for(let i = 0; i < validate.errors.length; i++) {
-    if(validate.errors[i].message != 'should NOT have additional properties') {
-        console.log(validate.errors[i]);
-    }
-}
 console.timeEnd("Execution time");
+console.log(findUnrecordedErrors(resourceToArrayOfKeys(resourceToBeValidated), schemaToArrayOfKeys(schema, resourceToBeValidated), getUniqueErrors(validate.errors)));
