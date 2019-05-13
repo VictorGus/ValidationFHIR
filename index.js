d0d0b38cf78e11a27756885e83c2d1ab181d800c
VictorGus <retaow@gmail.com>
2019-05-13 18:54:52 +0300

    Error management improvements

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 var Ajv = require('ajv');
 const fs = require('fs');
-var ajv = new Ajv({allErrors:true});
+const ValidationErrorsManager = require('./lib/errormanagement.js');
+var ajv = new Ajv();
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
 console.time("Execution time");
 const jsonFile = fs.readFileSync("/home/victor/Downloads/fhir.schema.json");
 const schema = JSON.parse(jsonFile);
 
+
 const isResourceTypePresented = (resourceProperties) => resourceProperties.includes('resourceType');
 
-function makeValidationError(errorType, param) {
-    const newValidationError = {
-        keyword: errorType,
-        dataPath: '',
-        schemaPath: '#/additionalProperties',
-        params: { additionalProperty: param },
-        message: 'should NOT have additional properties' 
+function createValidationError(newMessage, errorType, errorParam,  invalidObject) {
+    if(Array.isArray(invalidObject)) {
+        var temp = [];
+        var arrayOfErrors = [];
+        for(let i in invalidObject) {
+            arrayOfErrors[i] = {
+                keyword: errorType,
+                dataPath: '',
+                schemaPath: `#/${errorType}`,
+                message: newMessage
+            }
+            temp[i]={}
+            temp[i][`${errorParam}`] = invalidObject[i];
+            arrayOfErrors[i]['params'] = temp[i];
+        }
+        return arrayOfErrors;
+    } else {
+        var newValidationError = {
+            keyword: errorType,
+            dataPath: '',
+            schemaPath: `#/${errorType}`,
+            message: newMessage
+        }
+        const temp = {};
+        temp[`${errorParam}`] = invalidObject;
+        newValidationError['params'] = temp;
+        return newValidationError;
     }
 }
 
@@ -57,29 +79,35 @@ function addUnrecordedErrors(unrecordedErrors, recordedErrors) {
     return recordedErrors;
 }
 
-function findUnrecordedErrors(resource, schema, arrayOfErrors) {
+function findUnrecordedErrors(resourceKeys, schema, arrayOfErrors) {
     var errorParams = [];
     for(let i = 0; i < arrayOfErrors.length; i++) {
         errorParams[i] = arrayOfErrors[i].params.additionalProperty;
     }
-    return resource.filter(property => !schema.includes(property) && !errorParams.includes(property)); 
+    const filteredParams = resourceKeys.filter(property => !schema.includes(property) && !errorParams.includes(property) && property);
+    if(filteredParams.length > 0)  {
+        return createValidationError('should NOT have additional properties', 'additionalProperties', 'additionalProperty', filteredParams);
+    } else {
+        return 0;
+    }
+}
+
+function filterErrors(schemaKeys, arrayOfErrors) {
+    var filteredArray = getUniqueErrors(arrayOfErrors);
+    return filteredArray.filter(element => !schemaKeys.includes(element.params.additionalProperty) && element);
 }
 
 const resourceJSON = fs.readFileSync('/home/victor/Documents/Diploma/FHIR validation app/Resources/claim.json');
 const resourceToBeValidated = JSON.parse(resourceJSON);
+const ValidationSchema = require('./lib/schema.js');
+const validationSchema = new ValidationSchema(schema, resourceToBeValidated);
+console.log(validationSchema.schemaAsArrayOfKeys);
 var validate = ajv.addSchema(schema).compile(schema);
 var valid = validate(resourceToBeValidated);
-console.log(validate.errors.length);
-if(!valid) {
-    var uniqueErrors = new Map();
-    for(let i = 0; i < validate.errors.length; i++) {
-        if(validate.errors[i].params.additionalProperty &&
-           !Object.keys(schema.definitions[`${resourceToBeValidated.resourceType}`].properties).includes(validate.errors[i].params.additionalProperty) && !uniqueErrors.has(validate.errors[i].params.additionalProperty)) {
-            uniqueErrors.set(validate.errors[i].params.additionaLProperty, validate.errors[i]);
-        }
-    }
-    console.log(uniqueErrors.values());
+if (!valid) {
+
+    const validationErrorManager = new ValidationErrorsManager(validationErrorManager.errors, )
+    validate.errors = filterErrors(schemaToArrayOfKeys(schema, resourceToBeValidated), validate.errors);
+    console.log(addUnrecordedErrors(findUnrecordedErrors(resourceToArrayOfKeys(resourceToBeValidated), schemaToArrayOfKeys(schema, resourceToBeValidated), validate.errors), validate.errors));
 }
 console.timeEnd("Execution time");
-console.log(findUnrecordedErrors(resourceToArrayOfKeys(resourceToBeValidated), schemaToArrayOfKeys(schema, resourceToBeValidated), getUniqueErrors(validate.errors)));
-console.log(isResourceTypePresented(resourceToArrayOfKeys(resourceToBeValidated)));
