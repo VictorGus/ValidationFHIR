a42cbd35e5f4c44b8db5801fd97b93d55bfb5a3a
VictorGus <retaow@gmail.com>
2019-05-22 14:28:54 +0300

    New tests and test cases

---
+++ test/Cases/Resources/invalidservicerequest.js
@@ -0,0 +1,18 @@
+{
+            "resourceType": "ServiceRequest",
+            "id": "referral",
+	    "test": 1,
+            "status": "completed",
+            "intent": "invalid",
+            "subject": {
+              "reference": "urn:uuid:d9cb1481-35fe-466e-9f73-266d78e559d4"
+            },
+            "requester": {
+              "reference": "urn:uuid:1842c39c-a236-44cf-8cd9-b05e4f74af9f"
+            },
+            "performer": [
+              {
+                "reference": "urn:uuid:1842c39c-a236-44cf-8cd9-b05e4f74af9f"
+              }
+            ]
+          }
--- test/validationtest.js
+++ test/validationtest.js
@@ -2,11 +2,87 @@ var assert = require('assert');
 var Validation = require('../validator.js');
 const validation = new Validation();
 
+describe('Validation correctness checking', function(){
    describe('#validateResource()', function() {
-        it('should return false when resource is not valid', function() {
-            assert.equal(validation.validateResource(__dirname + '/Cases/Resources/claim.json'), false);
-        }).timeout(2500);
-       it('should return true when resource is valid', function() {
-           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/patient.json'), true);
+       it('should return true when Patient resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/patient.json', false), true);
        }).timeout(2500);
-    });
+
+       it('should return true when Procedure resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/procedure.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Observation resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/observation.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Bundle resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/bundle.json', false), true);
+       }).timeout(2500);
+       
+       it('should return true when Orginization resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/organization.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when MedicationRequest resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/medicationrequest.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when DiagnosticReport resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/diagnosticreport.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Careplan resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/careplan.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Condition resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/condition.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Encounter resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/encounter.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Immunization resource is valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/immunization.json', false), true);
+       }).timeout(2500);
+
+       it('should return true when Patient resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidpatient.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when Claim resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/claim.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when ServiceRequest resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/servicerequest.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when Procedure resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidprocedure.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when Observation resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidobservation.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when MedicationRequest resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidmedicationrequest.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when MedicationRequest resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidmedicationrequest.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when ServiceRequest resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidservicerequest.json', false), false);
+       }).timeout(2500);
+
+       it('should return false when Bundle resource is not valid', function() {
+           assert.equal(validation.validateResource(__dirname + '/Cases/Resources/invalidbundle.json', false), false);
+       }).timeout(2500);
+
+   });
+});
